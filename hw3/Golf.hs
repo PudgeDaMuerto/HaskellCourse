8399ad2429442ee46a6002eb0b3083c2133f5fa8
George 228 <maxkostenko2001@mail.ru>
2023-08-07 15:18:27 +0300

    homework completed

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,9 +1,13 @@
 module Golf () where
-import Data.List (group, transpose)
+import Data.List (transpose)
+
+-----------------------------Ex.1 Hopscotch-----------------------------
 
 skips :: [a] -> [[a]]
 skips [] = []
 skips xs = [[x | (i, x) <- zip [1..] xs, i `mod` n == 0] | n <- [1..length xs]]
+
+---------------------------Ex.2 Local maxima----------------------------
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima [_, _] = []
@@ -11,23 +15,14 @@ localMaxima (x:y:z:xs)
     | x < y && y > z = y : localMaxima (y:z:xs)
     | otherwise = localMaxima (y:z:xs)
 
+-----------------------------Ex.3 Histogram-----------------------------
+
 histogram :: [Integer] -> String
--- histogram = foldr (\x acc -> replicate (fromIntegral x) ' ' ++ "*\n" ++ acc) "==========\n0123456789\n"
-histogram xs = unlines [replicate (fromIntegral x) ' ' ++ "*" | x <- xs] ++ legend 
+histogram xs = unlines $ rotate bars ++ legend
     where
+        count x = length . filter (==x)
+        template = [count i xs | i <- [0..9]]
         rotate = reverse . transpose
-        legend = "==========\n0123456789\n"
-
-rotate :: [[a]] -> [[a]]
-rotate = reverse . transpose
-
-bars :: [[String]] -> [String]
-bars xs = [x | x <- xs]
-
-drawOne :: Integer -> String
-drawOne x = replicate (fromIntegral x) ' ' ++ "\n"
-
-draw :: [Integer] -> String
-draw xs@(x:t)
-    | length xs == 1 = drawOne x
-    | otherwise =  
+        max = maximum template
+        bars = map (\x -> take max $ replicate x '*' ++ repeat ' ') template
+        legend = ["==========","0123456789"]
